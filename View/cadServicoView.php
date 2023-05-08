56c353e33a5bfd216500410a2b27ed17ccc2b5dc
Rangel Adonai <108149032+rangeladonai@users.noreply.github.com>
2023-05-08 18:08:25 -0300

    Update cadServicoView.php

--- View/cadServicoView.php
+++ View/cadServicoView.php
@@ -1,7 +1,8 @@
 <?php
+session_start();
 require_once '../Templates/header.php';
 require '../Model/connection.php';
-session_start();
+
 ?>
 <!DOCTYPE html>
 <!-- Coding by CodingLab | www.codinglabweb.com -->
